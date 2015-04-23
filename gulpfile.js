223a1f252b61a1b49fac4d05bc0574da5c8c4b7e
armorik83 <okunokentaro@me.com>
2015-04-23 14:53:48 +0900

    chore(gulp): for building

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var shell = require('gulp-shell');
 var mocha = require('gulp-mocha');
 
 var opt = {
+  bin:           './bin',
   lib:           './lib',
   src:           './src',
   test:          './test',
@@ -14,10 +15,11 @@ var opt = {
 
 /* clean */
 gulp.task('clean', del.bind(null, [
-  opt.src  + '/**/*.js',
-  opt.src  + '/**/*.js.map',
-  opt.test + '/**/*.js',
-  opt.test + '/**/*.js.map',
+  `${opt.src }/**/*.js`,
+  `${opt.src }/**/*.js.map`,
+  `${opt.test}/**/*.js`,
+  `${opt.test}/**/*.js.map`,
+  opt.lib,
   opt.testEspowered
 ]));
 
@@ -31,30 +33,41 @@ gulp.task('ts',      function(done) {seq('clean', ['ts:src_'], done)});
 gulp.task('babel:test', shell.task([`babel ${opt.test} --plugins babel-plugin-espower --out-dir ${opt.testEspowered}`]));
 
 /* watch */
-gulp.task('exec-watch', ['test'], function() {
+gulp.task('exec-watch:test', ['test'], function() {
   gulp.watch([`${opt.src}/**/*.ts`, `${opt.test}/**/*.es6`], ['test'])
     .on('error', function(err) {
       process.exit(1);
     });
 });
-
-gulp.task('watch', function() {
-  var spawn = function() {
-    var proc = require('child_process').spawn('gulp', ['exec-watch'], {stdio: 'inherit'});
-    proc.on('close', function(c) {
-      spawn();
+gulp.task('exec-watch:cli', ['build:src'], function() {
+  gulp.watch([`${opt.src}/**/*.ts`], ['build:src'])
+    .on('error', function(err) {
+      process.exit(1);
     });
-  };
-  spawn();
 });
+
+
+function watch(target) {
+  return function() {
+    var spawn = function() {
+      var proc = require('child_process').spawn('gulp', [`exec-watch:${target}`], {stdio: 'inherit'});
+      proc.on('close', function(c) {
+        spawn();
+      });
+    };
+    spawn();
+  };
+}
+
+gulp.task('watch:test', watch('test'));
+gulp.task('watch:cli',  watch('cli'));
 
 /* build */
 gulp.task('copy:src', function() {
   gulp.src(`${opt.src}/**/*.js`)
     .pipe(gulp.dest(opt.lib));
 });
-gulp.task('build:src', function(done) {seq(['clean:lib', 'ts:src'], 'copy:src', done)});
-gulp.task('build',     function(done) {seq('build:src', done)});
+gulp.task('build:src', function(done) {seq(['clean', 'ts:src'], 'copy:src', done)});
 
 /* test */
 gulp.task('mocha', function() {
