c4ee98c432641383de585f2a3bca56b45d8f803a
armorik83 <okunokentaro@me.com>
2015-05-08 12:53:22 +0900

    chore(cw-htmlconv.d.ts): update

--- cw-htmlconv.d.ts
+++ cw-htmlconv.d.ts
@@ -1,25 +1,29 @@
-/// <reference path="../es6-promise/es6-promise.d.ts" />
-
 declare module cwHtmlconv {
-  interface ConvertMethodDefinition {
-    method: string;
-    newAttribute: string;
-    open: string;
-    close: string;
-    separator: string;
-    valuePattern: string;
-    newValue: string;
+  interface PatternObject {
+    selector:      string;
+    attrPattern?:  string;
+    attrReplace?:  string;
+    attrRemove?:   boolean;
+    valuePattern?: string;
+    valueReplace?: string;
+    textPattern?:  string;
+    textReplace?:  string;
+    valueEmpty?:   boolean;
+    method?: {
+      behavior: string;
+      open:     string;
+      start:    string;
+      end:      string;
+    };
+    manipulation?: Array<{
+      type:   string;
+      match:  number;
+      filter: string;
+    }>;
+    subPatterns?: PatternObject[];
   }
 
-  type PatternsForValue = {[pattern: string]: string};
-  type PatternsForAttr = {[pattern: string]: string|[string, PatternsForValue]|ConvertMethodDefinition};
-
-  /**
-   * @param {string}          input
-   * @param {PatternsForAttr} patterns
-   * @returns {Promise<string>}
-   */
-  export default function main(input: string, patterns?: PatternsForAttr): Promise<string>;
+export default function main(input: string, patterns?: PatternObject[]): string;
 }
 
 declare module 'cw-htmlconv' {
